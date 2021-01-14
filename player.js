05ed8d07c73dbd9f432e5f462d59627e5c077923
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-14 10:45:44 -0700

    Begin the playCard() method for Player class

---
+++ player.js
@@ -0,0 +1,18 @@
+
+class Player {
+  constructor(id) {
+    this.hand = [];
+    this.wins = 0;
+    this.id = id;
+  }
+  playCard() {
+    var currentCard = this.hand.splice(-1);
+    //push currentCard into middle deck array in Game class
+    //will build out more functionality with Game class
+    return currentCard;
+
+  }
+  saveToStorage() {
+
+  }
+}
