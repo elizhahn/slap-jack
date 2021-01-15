18dd813fd982e3c0bd2978fb1b6a5c7c7327eaac
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-15 11:37:25 -0700

    Add method to shuffle player deck

--- player.js
+++ player.js
@@ -16,4 +16,14 @@ class Player {
     var player = JSON.stringify(this);
     localStorage.setItem(this.id, player)
   }
+  shufflePlayerDeck() {
+    var shuffledCards = [];
+    var playerCardAmount = this.hand.length;
+    for(var i = 0; i < playerCardAmount; i++) {
+      var index = Math.floor(Math.random() * this.hand.length)
+      var randomCard = this.hand.splice(index, 1);
+      shuffledCards.push(randomCard[0])
+  }
+    this.hand = shuffledCards; 
+ }
 }
