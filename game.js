72a2ae9a72cb28252f782a97632f7ff1b72e40b3
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-15 10:31:11 -0700

    Create suites object for slap conditions

--- game.js
+++ game.js
@@ -100,13 +100,28 @@ class Game {
     //if slap hits a Jack
     //middle pile goes to player who caused the keypress event
     //return true
+    var suits = {
+  "1" : ["card-deck-assets/gold-01.png", "card-deck-assets/blue-01.png", "card-deck-assets/green-01.png","card-deck-assets/red-01.png"],
+  "2" : ["card-deck-assets/blue-02.png", "card-deck-assets/gold-02.png", "card-deck-assets/green-02.png", "card-deck-assets/red-02.png"],
+  "3" : [ "card-deck-assets/blue-03.png", "card-deck-assets/gold-03.png", "card-deck-assets/green-03.png", "card-deck-assets/red-03.png"],
+  "4" : ["card-deck-assets/blue-04.png", "card-deck-assets/gold-04.png", "card-deck-assets/green-04.png", "card-deck-assets/red-04.png"],
+  "5" : ["card-deck-assets/blue-05.png", "card-deck-assets/gold-05.png", "card-deck-assets/green-05.png", "card-deck-assets/red-05.png"],
+  "6" : ["card-deck-assets/gold-06.png", "card-deck-assets/blue-06.png", "card-deck-assets/green-06.png", "card-deck-assets/red-06.png"],
+  "7" : ["card-deck-assets/gold-07.png", "card-deck-assets/blue-07.png", "card-deck-assets/green-07.png", "card-deck-assets/red-07.png"],
+  "8" : ["card-deck-assets/blue-08.png", "card-deck-assets/gold-08.png", "card-deck-assets/green-08.png", "card-deck-assets/red-08.png"],
+  "9" : ["card-deck-assets/blue-09.png", "card-deck-assets/gold-09.png", "card-deck-assets/green-09.png", "card-deck-assets/red-09.png"],
+  "10" :["card-deck-assets/blue-10.png", "card-deck-assets/gold-10.png", "card-deck-assets/green-10.png", "card-deck-assets/red-10.png"],
+  "jack":["card-deck-assets/blue-jack.png", "card-deck-assets/gold-jack.png", "card-deck-assets/green-jack.png", "card-deck-assets/red-jack.png"],
+  "queen" :["card-deck-assets/blue-queen.png","card-deck-assets/gold-queen.png","card-deck-assets/green-queen.png", "card-deck-assets/red-queen.png"],
+  "king" : ["card-deck-assets/blue-king.png", "card-deck-assets/gold-king.png", "card-deck-assets/green-king.png", "card-deck-assets/red-king.png"]
+  }
+    var cardsWon = this.middlePile.splice(0)
     var blueJack = "card-deck-assets/blue-jack.png";
     var redJack = "card-deck-assets/red-jack.png";
     var goldJack = "card-deck-assets/gold-jack.png";
     var greenJack = "card-deck-assets/green-jack.png";
     var length = this.middlePile.length
-     if(this.middlePile[length - 1] === blueJack || this.middlePile[length - 1] === redJack || this.middlePile[length - 1] === goldJack || this.middlePile[length - 1] === greenJack) {
-      var cardsWon = this.middlePile.splice(0)
+    if(this.middlePile[length - 1] === blueJack || this.middlePile[length - 1] === redJack || this.middlePile[length - 1] === goldJack || this.middlePile[length - 1] === greenJack) {
       if(this.currentPlayer === 0) {
         this.players[0].hand = [...this.players[0].hand, ...cardsWon];
       } else {
@@ -114,6 +129,18 @@ class Game {
       }
       return true;
     }
+  
+
+      //if a double appears
+      //
+
+      //if a sandwich appears
+
+      //invalid slap
+      //top card of player who slapped will go to other player
+      //player1.hand[-1];
+  }
+}
 
 
     //if a double appears
@@ -124,7 +151,6 @@ class Game {
     //invalid slap
     //top card of player who slapped will go to other player
     //player1.hand[-1];
-  }
-}
+
 
 var game = new Game();
