bbde3510dd40a31f170c3ff5317e3c4f53bc692a
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-16 15:28:48 -0700

    Clean up comments and formatting

--- main.js
+++ main.js
@@ -20,22 +20,13 @@ currentGame.dealCards();
 
 
 //EVENT HANDLERS-----------------------------
-//player losing all of their cards
-    // create function that will check the player's decks before playCard() is called
-    //if a player's hand is empty the current player will always be set to the other player
-    // function checkHand() {
-        //if(currentGame.player[0].hand.length === 0)
-           //currentGame.currentPlayer = 1;
-        //if(currentGame.player[1].hand.length === 0)
-            //currentGame.currentPlayer = 0;
-
 function checkHand() {
   if(currentGame.players[0].hand.length === 0) {
     currentGame.currentPlayer = 1;
 } else if(currentGame.players[1].hand.length === 0) {
     currentGame.currentPlayer = 0;
 }
-}
+};
 
 //Function to validate and execute any play Action
 function validatePlayAction(event) {
@@ -53,22 +44,16 @@ function validatePlayAction(event) {
      currentGame.playCard();
       return true;
   } else if (event.keyCode === 74) {
-      currentGame.whoSlapped = 1
+      currentGame.whoSlapped = 1;
       currentGame.slap();
       return true;
   } else {
     return false;
   }
-}
+};
 
 
 function playCard() {
   checkHand();
   validatePlayAction(event);
-}
-
-
-
-
-// Player has one chance to win, must slap a jack? double check, also have to figure out how
-//to tell if pile was rotated through once
+};
