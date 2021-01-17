f5caa47d7d9c8f5a3d59019cd1f5bffd210af5ad
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-16 20:31:00 -0700

    Function for zero cards condition, prevents that player from dealing

--- main.js
+++ main.js
@@ -23,9 +23,12 @@ currentGame.dealCards();
 function checkHand() {
   if(currentGame.players[0].hand.length === 0) {
     currentGame.currentPlayer = 1;
+    return true;
 } else if(currentGame.players[1].hand.length === 0) {
     currentGame.currentPlayer = 0;
+    return true;
 }
+  return false;
 };
 
 //Function to validate and execute any play Action
@@ -54,6 +57,39 @@ function validatePlayAction(event) {
 
 
 function playCard() {
-  checkHand();
+  console.log("TEST");
+  if(!checkHand()) {
   validatePlayAction(event);
+  console.log(currentGame.middlePile);
+} else {
+  lastPlay(event);
+}
 };
+
+//Player loses all cards
+// Have a checkHand function that currently checks if each player's hand is empty
+//if a player's hand is empty, the current player is always going to be set to the other players
+//the losing player has one chance to get back into the Game
+   //They can only get back in the game if they successfully slap a jack and only a jack
+
+//if checkHand returns this function is called
+function lastPlay(event) {
+  var playerHand0 = currentGame.players[0].hand;
+  var playerHand1 = currentGame.players[1].hand;
+  //player 0
+  if(event.keyCode === 81 && currentGame.currentPlayer === 0) {
+      currentGame.playCard();
+      currentGame.currentPlayer = 0
+  } else if(event.keyCode === 70 && playerHand1.length === 0) {
+      currentGame.whoSlapped = 0
+  }
+  //player 1
+   else if (event.keyCode === 80 && currentGame.currentPlayer === 1) {
+     currentGame.playCard();
+       currentGame.currentPlayer = 1
+  } else if (event.keyCode === 74 && playerHand0.length === 0) {
+      currentGame.whoSlapped = 1;
+  } else {
+    return false;
+  }
+}
