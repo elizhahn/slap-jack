a4a5303d325f6517be6f7cb0b4fe76c30770b419
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-17 08:20:31 -0700

    Refactor lastPlay function

--- main.js
+++ main.js
@@ -66,61 +66,153 @@ function playCard() {
 }
 };
 
-//Player loses all cards
-// Have a checkHand function that currently checks if each player's hand is empty
-//if a player's hand is empty, the current player is always going to be set to the other players
-//the losing player has one chance to get back into the Game
-   //They can only get back in the game if they successfully slap a jack and only a jack
 
 //if checkHand returns this function is called
+// function lastPlay(event) {
+//   var playerHand0 = currentGame.players[0].hand;
+//   var playerHand1 = currentGame.players[1].hand;
+//   //player 0
+//   if(event.keyCode === 81 && currentGame.currentPlayer === 0) {
+//       currentGame.playCard();
+//       if(playerHand0.length === 0) {
+//        var cards = currentGame.middlePile.splice(0);
+//        currentGame.players[0].hand = cards;
+//      }
+//       currentGame.players[0].shufflePlayerDeck();
+//       currentGame.currentPlayer = 0
+//   } else if(event.keyCode === 70 && playerHand1.length === 0) {
+//       currentGame.whoSlapped = 0
+//       if(currentGame.slapJack()) {
+//         currentGame.players[0].wins++;
+//       } else {
+//         currentGame.invalidSlap();
+//       }
+//   }
+//   //player 1
+//    else if (event.keyCode === 80 && currentGame.currentPlayer === 1) {
+//      currentGame.playCard();
+//        if(playerHand1.length === 0) {
+//          var cards = currentGame.middlePile.splice(0);
+//          currentGame.players[1].hand = cards
+//       }
+//        currentGame.players[1].shufflePlayerDeck();
+//        currentGame.currentPlayer = 1
+//   } else if (event.keyCode === 74 && playerHand0.length === 0) {
+//       currentGame.whoSlapped = 1;
+//         if(currentGame.slapJack()) {
+//           currentGame.players[0].wins++;
+//         } else {
+//           currentGame.invalidSlap();
+//         }
+//   } else {
+//     return false;
+//   }
+// }
+
 function lastPlay(event) {
+  if(event.keyCode === 81) {
+    winnerDealsPlayer0();
+} else if( event.keyCode === 80) {
+    winnerDealsPlayer1();
+} else if(event.keyCode === 70) {
+    winningSlapPlayer0();
+} else if(event.keyCode === 74) {
+    winningSlapPlayer1();
+}
+}
+
+//Refactor lastPlay()
+// function winnerDeals() {
+// if(event.keyCode === 81 && currentGame.currentPlayer === 0) {
+//     currentGame.playCard();
+//     if(playerHand0.length === 0) {
+//      var cards = currentGame.middlePile.splice(0);
+//      currentGame.players[0].hand = cards;
+//    }
+//     currentGame.players[0].shufflePlayerDeck();
+//     currentGame.currentPlayer = 0
+// } else if (event.keyCode === 80 && currentGame.currentPlayer === 1) {
+//    currentGame.playCard();
+//      if(playerHand1.length === 0) {
+//        var cards = currentGame.middlePile.splice(0);
+//        currentGame.players[1].hand = cards
+//     }
+//      currentGame.players[1].shufflePlayerDeck();
+//      currentGame.currentPlayer = 1
+// }
+// }
+
+//refactor winnerDeals()
+function winnerDealsPlayer0() {
   var playerHand0 = currentGame.players[0].hand;
+  currentGame.playCard();
+    if(playerHand0.length === 0) {
+       var cardsWon = currentGame.middlePile.splice(0);
+       currentGame.players[0].hand = cardsWon;
+       currentGame.players[0].shufflePlayerDeck();
+
+   }
+  currentGame.currentPlayer = 0
+}
+
+//refactor winnerDeals()
+function winnerDealsPlayer1() {
   var playerHand1 = currentGame.players[1].hand;
-  //player 0
-  if(event.keyCode === 81 && currentGame.currentPlayer === 0) {
-      currentGame.playCard();
-      if(playerHand0.length === 0) {
-       var cards = currentGame.middlePile.splice(0);
-       currentGame.players[0].hand = cards;
-     }
-      currentGame.players[0].shufflePlayerDeck();
-      currentGame.currentPlayer = 0
-  } else if(event.keyCode === 70 && playerHand1.length === 0) {
-      currentGame.whoSlapped = 0
+  currentGame.playCard();
+    if(playerHand1.length === 0) {
+      var cardsWon = currentGame.middlePile.splice(0);
+      currentGame.players[1].hand = cardsWon;
+      currentGame.players[1].shufflePlayerDeck();
+    }
+
+  currentGame.currentPlayer = 1
+}
+
+//refactor lastPlay()
+// function winningSlap() {
+//    if(event.keyCode === 70 && playerHand1.length === 0) {
+//       currentGame.whoSlapped = 0
+//       if(currentGame.slapJack()) {
+//         currentGame.players[0].wins++;
+//       } else {
+//         currentGame.invalidSlap();
+//       }
+//  } else if (event.keyCode === 74 && playerHand0.length === 0) {
+//       currentGame.whoSlapped = 1;
+//         if(currentGame.slapJack()) {
+//           currentGame.players[0].wins++;
+//         } else {
+//           currentGame.invalidSlap();
+//         }
+//     }
+//   }
+
+  //refactor winningSlap()
+  function winningSlapPlayer0() {
+    currentGame.whoSlapped = 0
+    if(currentGame.slapJack()) {
+      currentGame.players[0].wins++;
+    } else {
+      currentGame.invalidSlap();
+    }
+  }
+
+  function winningSlapPlayer1() {
+    currentGame.whoSlapped = 1;
       if(currentGame.slapJack()) {
         currentGame.players[0].wins++;
       } else {
         currentGame.invalidSlap();
       }
   }
-  //player 1
-   else if (event.keyCode === 80 && currentGame.currentPlayer === 1) {
-     currentGame.playCard();
-       if(playerHand1.length === 0) {
-         var cards = currentGame.middlePile.splice(0);
-         currentGame.players[1].hand = cards
-      }
-       currentGame.players[1].shufflePlayerDeck();
-       currentGame.currentPlayer = 1
-  } else if (event.keyCode === 74 && playerHand0.length === 0) {
-      currentGame.whoSlapped = 1;
-        if(currentGame.slapJack()) {
-          currentGame.players[0].wins++;
-        } else {
-          currentGame.invalidSlap();
-        }
-  } else {
-    return false;
-  }
-}
 
-// If losing player slaps a jacks
-function redemptionSlap(event) {
-  if(event.keycode === 70 && currentPlayer[0].hand === 0 && currentGame.slapJack()) {
-    var cardsWon = currentGame.middlePile.splice(0);
-    currentGame.players[0].hand = cards;
-  } else if(event.keycode === 74 && currentPlayer[1].hand === 0 && currentGame.slapJack()) {
-    var cardsWon = currentGame.middlePile.splice(0);
-    currentGame.players[1].hand = cards;
-  }
-  }
+  //If losing player slaps a jacks
+  function redemptionSlap(event) {
+    if(event.keycode === 70 && currentPlayer[0].hand === 0 && currentGame.slapJack()) {
+      var cardsWon = currentGame.middlePile.splice(0);
+      currentGame.players[0].hand = cards;
+    } else if(event.keycode === 74 && currentPlayer[1].hand === 0 && currentGame.slapJack()) {
+      var cardsWon = currentGame.middlePile.splice(0);
+      currentGame.players[1].hand = cards;
+    }
+    }
